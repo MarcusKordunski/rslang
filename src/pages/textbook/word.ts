4a6f1ecd26cde42a0ac92f1a42932102bbbd96fb
Mihail Samofalov <rhbkickshot@gmail.com>
2022-08-19 12:23:51 +0300

    feat: add word class

---
+++ src/pages/textbook/word.ts
@@ -0,0 +1,73 @@
+import { IWord } from "../../types/types";
+import create from "../../utils/create";
+
+export class Word {
+
+  public id: string;
+  public group: number;
+  public page: number;
+  public word: string;
+  public image: string;
+  public audio: string;
+  public audioMeaning: string;
+  public audioExample: string;
+  public textMeaning: string;
+  public textExample: string;
+  public transcription: string;
+  public wordTranslate: string;
+  public textMeaningTranslate: string;
+  public textExampleTranslate: string;
+  public wordContainer: HTMLElement;
+
+  constructor(word: IWord) {
+    this.id = word.id;
+    this.group = word.group;
+    this.page = word.page;
+    this.word = word.word;
+    this.image = word.image;
+    this.audio = word.audio;
+    this.audioMeaning = word.audioMeaning;
+    this.audioExample = word.audioExample;
+    this.textMeaning = word.textMeaning;
+    this.textExample = word.textExample;
+    this.textMeaningTranslate = word.textMeaningTranslate;
+    this.textExampleTranslate = word.textExampleTranslate;
+    this.wordTranslate = word.wordTranslate;
+    this.transcription = word.transcription;
+
+    this.wordContainer = create('div', 'textbook__word word-card');
+
+    const wordImgBox: HTMLElement = create('div', 'word-card__img-box', this.wordContainer);
+    const wordImg: HTMLElement = create('img', 'word-card__img', wordImgBox, undefined, ['src', ``], ['alt', `${this.word}`]);
+
+    const wordBody: HTMLElement = create('div', 'word-card__body', this.wordContainer);
+
+    const wordTop: HTMLElement = create('div', 'word-card__top', wordBody);
+    const wordTitle: HTMLElement = create('div', 'word-card__title-box', wordTop);
+    const wordAudio: HTMLElement = create('div', 'word-card__audio', wordTop);
+
+    const wordTitleRowTop: HTMLElement = create('div', 'word-card__title-row', wordTitle);
+    const wordTitleRowBottom: HTMLElement = create('div', 'word-card__title-row', wordTitle);
+
+    const wordName: HTMLElement = create('div', 'word-card__word', wordTitleRowTop);
+    wordName.textContent = `${this.word}`;
+    const wordTanscription: HTMLElement = create('div', 'word-card__transcription', wordTitleRowTop);
+    wordTanscription.textContent = `${this.transcription}`;
+    const wordTranslate: HTMLElement = create('div', 'word-card__translate', wordTitleRowBottom);
+
+    const wordMid: HTMLElement = create('div', 'word-card__mid', wordBody);
+    const wordMeaning: HTMLElement = create('div', 'word-card__meaning', wordMid);
+    wordMeaning.textContent = `${this.textMeaning}`;
+    const wordMeaningTranslate: HTMLElement = create('div', 'word-card__meaning-translate', wordMid);
+    wordMeaningTranslate.textContent = `${this.textMeaningTranslate}`;
+
+    const wordBottom: HTMLElement = create('div', 'word-card__bottom', wordBody);
+    const wordExample: HTMLElement = create('div', 'word-card__example', wordMid);
+    wordExample.textContent = `${this.textExample}`;
+    const wordExampleTranslate: HTMLElement = create('div', 'word-card__example-translate', wordMid);
+    wordExampleTranslate.textContent = `${this.textExampleTranslate}`;
+
+    const wordTracker: HTMLElement = create('div', 'word-card__tracker', wordBody);
+    const wordButtons: HTMLElement = create('div', 'word-card__buttons', wordBody);
+  }
+}
--- src/types/types.ts
+++ src/types/types.ts
@@ -18,3 +18,20 @@ export interface IHeader {
 export interface INavigation {
   getHtml: () => string;
 }
+
+export interface IWord {
+  id: string;
+  group: number;
+  page: number;
+  word: string;
+  image: string;
+  audio: string;
+  audioMeaning: string;
+  audioExample: string;
+  textMeaning: string;
+  textExample: string;
+  transcription: string;
+  wordTranslate: string;
+  textMeaningTranslate: string;
+  textExampleTranslate: string;
+}
