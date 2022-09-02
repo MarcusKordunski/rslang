f8c6480ef9e7e37131194664664aa426b37095b1
Mihail Samofalov <rhbkickshot@gmail.com>
2022-09-02 16:07:57 +0300

    feat: add footer info and games adaptive

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -2,13 +2,13 @@ export class Footer {
   getHtml(): string {
     return `
       <div class='footer-container container'>
-        <div class='logo-rss'></div>
+        <div class='logo-rss'><a href="https://rs.school/js/"><img src='./assets/icons/rs_school_js.svg' alt='rsschool'></a></div>
         <div class='creators'>
-          <div class='creators-item'></div>
-          <div class='creators-item'></div>
-          <div class='creators-item'></div>
+          <a class='creators-item' href='https://github.com/zem41k'>Mihail</a>
+          <a class='creators-item' href='https://github.com/antipachita'>Vladimir</a>
+          <a class='creators-item' href='https://github.com/MarcusKordunski'>Marcus</a>
         </div>
-        <div class="project"></div>
+        <div class="project">2022</div>
       </div>`
   }
 }
